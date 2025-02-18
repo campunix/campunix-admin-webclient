7008efd73629475c4b4d7fde07411d709175ee79
rasel.ahammed <raselcse20@gmail.com>
2025-02-18 21:26:41 +0600

    Restore session on reload functionality added

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -77,62 +77,46 @@ export class AuthService {
                 // Store the access token in the local storage
                 this.accessToken = response.access_token;
 
-                // Set the authenticated flag to true
-                this._authenticated = true;
-
-                // Store the user on the user service
-                // this._userService.user = response.user;
-
-                this.getUser().subscribe((user) => {
-                    this._userService.user = user;
-                });
-
                 // Return a new observable with the response
                 return of(response);
             }),
+            switchMap(() => this.getUserDetails())
         );
-    }
-
-    getUser(): Observable<any> {
-        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.accessToken}`);
-        return this._httpClient.get(`${environment.apiUrl}/me`, {headers});
     }
 
     /**
      * Sign in using the access token
      */
-    signInUsingToken(): Observable<any> {
-        // Sign in using the token
-        return this._httpClient.post('api/auth/sign-in-with-token', {
-            accessToken: this.accessToken,
-        }).pipe(
-            catchError(() =>
+    getUserDetails(): Observable<any> {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.accessToken}`);
+        return this._httpClient.get(`${environment.apiUrl}/me`, {headers})
+            .pipe(
+                catchError(() =>
+                    // Return false
+                    of(false),
+                ),
+                switchMap((response: any) => {
+                    // Replace the access token with the new one if it's available on
+                    // the response object.
+                    //
+                    // This is an added optional step for better security. Once you sign
+                    // in using the token, you should generate a new one on the server
+                    // side and attach it to the response object. Then the following
+                    // piece of code can replace the token with the refreshed one.
+                    if (response.accessToken) {
+                        this.accessToken = response.accessToken;
+                    }
 
-                // Return false
-                of(false),
-            ),
-            switchMap((response: any) => {
-                // Replace the access token with the new one if it's available on
-                // the response object.
-                //
-                // This is an added optional step for better security. Once you sign
-                // in using the token, you should generate a new one on the server
-                // side and attach it to the response object. Then the following
-                // piece of code can replace the token with the refreshed one.
-                if (response.accessToken) {
-                    this.accessToken = response.accessToken;
-                }
+                    // Set the authenticated flag to true
+                    this._authenticated = true;
 
-                // Set the authenticated flag to true
-                this._authenticated = true;
+                    // Store the user on the user service
+                    this._userService.user = response;
 
-                // Store the user on the user service
-                this._userService.user = response.user;
-
-                // Return true
-                return of(true);
-            }),
-        );
+                    // Return true
+                    return of(true);
+                }),
+            );
     }
 
     /**
@@ -187,6 +171,6 @@ export class AuthService {
         }
 
         // If the access token exists, and it didn't expire, sign in using it
-        return this.signInUsingToken();
+        return this.getUserDetails();
     }
 }
